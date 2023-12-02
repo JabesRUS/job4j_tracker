c3fa1738df250e509eb81ffbafdff8f8e9d33960
JabesRus_Asus <jabesgames@gmail.com>
2023-12-02 07:01:09 +0300

    2.1. Рефакторинг - Площадь треугольника. [#122642]

---
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.oop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.offset;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TriangleTest {
+
+    @Test
+    public void when00and40and04Then8() {
+        Point a = new Point(0, 0);
+        Point b = new Point(4, 0);
+        Point c = new Point(0, 4);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        double expected = 8;
+        assertThat(rsl).isCloseTo(expected, offset(0.001));
+    }
+
+    @Test
+    public void when11and22and33ThenMinus1() {
+        Point a = new Point(1, 1);
+        Point b = new Point(2, 2);
+        Point c = new Point(3, 3);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        double expected = -1;
+        assertThat(rsl).isCloseTo(expected, offset(0.001));
+    }
+
+}
