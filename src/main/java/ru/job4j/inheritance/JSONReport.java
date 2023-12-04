ecae0bd86fe80d387b4c60852d150682ef281104
JabesRus_Asus <jabesgames@gmail.com>
2023-12-04 23:12:32 +0300

    2.1. Рефакторинг - Площадь треугольника. [#122642]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -3,8 +3,9 @@ package ru.job4j.inheritance;
 public class JSONReport extends TextReport {
 
     public String generate(String name, String body) {
-        return "/"name/"" + name + "</h1>"
-                + "<br/>"
-                + "<span>" + body + "</span>";
+        return "{" + System.lineSeparator()
+                + "\t\"name\" : " + name + "," + System.lineSeparator()
+                + "\t\"body\" : " + body + "," + System.lineSeparator()
+                + "}";
     }
 }
