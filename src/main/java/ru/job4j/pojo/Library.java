6aef82833ccfe40dbcace7be2ebe07b2e3ac696b
JabesRus <jabesgames@gmail.com>
2023-12-06 18:13:30 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,12 @@
+package ru.job4j.pojo;
+
+public class Library {
+    Book physics = new Book();
+    Book chemistry = new Book();
+    Book mathematics = new Book();
+    Book cleanCode = new Book();
+
+    Product[] lib = new Product[4];
+
+
+}
